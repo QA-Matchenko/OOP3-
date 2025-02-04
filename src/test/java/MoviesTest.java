ba2bd4fbe043b6b4642a65bd63c5fd6a3da2d8aa
Darya M <matchenko.darya@mail.ru>
2025-02-04 13:19:56 +0300

    modified test

--- src/test/java/MoviesTest.java
+++ src/test/java/MoviesTest.java
@@ -57,7 +57,40 @@ public class MoviesTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void test5() {
+        MovieManager manager = new MovieManager();
 
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+        manager.addMovie("Film 7");
+
+        String[] expected = {"Film 7", "Film 6", "Film 5", "Film 4", "Film 3"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test6() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+
+
+        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4", "Film 5"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
 
 
